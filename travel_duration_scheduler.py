d306839494686794d2ccf9e83d6435a7873776c7
bsezen82 <30347823+bsezen82@users.noreply.github.com>
2025-05-08 14:57:29 +0300

    Update travel_duration_scheduler.py

--- travel_duration_scheduler.py
+++ travel_duration_scheduler.py
@@ -14,7 +14,7 @@ def load_data():
 
     df["Duration (min)"] = df["Travel Duration"].apply(lambda x: round(float(x) / 60, 1) if pd.notnull(x) else None)
     df["Distance (km)"] = df["Distance"] / 1000
-    df[["Origin Lat", "Origin Lng"]] = df["Origin Coords"].str.split(",", expand=True).astype(float)
+        df[["Origin Lat", "Origin Lng"]] = df["Origin Coords"].str.split(",", expand=True).astype(float)
     df[["Destination Lat", "Destination Lng"]] = df["Destination Coords"].str.split(",", expand=True).astype(float)
     return df
 
