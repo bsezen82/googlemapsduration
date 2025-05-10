4e538da4c8d605fa1fd12399f94f09c4edcd374c
bsezen82 <30347823+bsezen82@users.noreply.github.com>
2025-05-10 21:38:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ with col1:
 
 with col2:
     st.subheader(f"➡️ To Mescid-i Haram (avg. {to_haram_avg_distance:.1f} km)")
-    st.caption("(Includes routes to: Al Aziziyah, Al Andulus, Al Diyafah, Al Rusayfah, Kudai)")
+    st.caption("(Includes routes from: Al Aziziyah, Al Andulus, Al Diyafah, Al Rusayfah, Kudai)")
     st.metric("Average Duration (min)", f"{to_haram_overall:.1f}" if to_haram_overall else "N/A")
     chart = alt.Chart(to_haram_combined).mark_line(point=True).encode(
         x=alt.X("Hour", sort=list(to_haram_combined["Hour"].unique())),
