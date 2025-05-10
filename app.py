63849bbfe7bfa8321d91b63b51a919dbdf0ed83a
bsezen82 <30347823+bsezen82@users.noreply.github.com>
2025-05-10 22:03:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ with col1:
     x=alt.X("Date:O", title="Date"),  # use ordinal for categorical days
     y=alt.Y("Average Duration (min)", title="Avg Duration (min)"),
     tooltip=["Date", "Average Duration (min)"]
-    ).properties(title="📊 To Al Haram - Daily Average Duration",height=500).mark_bar(size=100)
+    ).properties(title="📊 To Al Haram - Daily Average Duration",height=300).mark_bar(size=50)
 
     st.altair_chart(bar_chart_from, use_container_width=True)
   
@@ -116,7 +116,7 @@ with col2:
     x=alt.X("Date:O", title="Date"),  # use ordinal for categorical days
     y=alt.Y("Average Duration (min)", title="Avg Duration (min)"),
     tooltip=["Date", "Average Duration (min)"]
-    ).properties(title="📊 From Al Haram - Daily Average Duration", height=500).mark_bar(size=100)
+    ).properties(title="📊 From Al Haram - Daily Average Duration", height=300).mark_bar(size=50)
 
     st.altair_chart(bar_chart_from, use_container_width=True)
 
